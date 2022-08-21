6a466a2a05086ebf9f160386a077954490399c59
Rasbats <miker@members.swis.net>
2022-08-21 18:12:07 +0100

    Update build-and-deploy.sh
    Use script from opencpn-manuals/main

--- ci/build-and-deploy.sh
+++ ci/build-and-deploy.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
-
+#
+# Build an website using Antora and deploy to Github Pages
+#
+# Configuration:
+#     Needs GITHUB_TOKEN in environment, usually using something like
+#         env:
+#           GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+#
 set -xe
+
+# Some hard-coded assumptions:
+readonly PLAYBOOK='site.yml'       # The Antora playbook
+readonly SITE_DIR='docs'           # Antora output directory
+readonly GP_BRANCH='gh-pages'      # Branch used as input by Github Pages
 
 # Install npm and antora
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 npm -v
-npm i -g @antora/cli@3.0.1
-npm i -g @antora/site-generator@3.0.1
-antora -v
+npm i
 
-# Update dependencies and build site
-antora site.yml
-touch docs/.nojekyll
+# Build site and block github's default jekyll formatting
+npm run build
+touch $SITE_DIR/.nojekyll
 
-# Set up a git environment in docs
-orig_subject=$(git log -1 --oneline $FETCH_HEAD)
+# Set up a git environment in $SITE_DIR
 author_email=$(git log -1 --pretty=format:"%ae")
-cd docs
-git init -b gh-pages
+cd $SITE_DIR
+git init -b pages
 git remote add origin \
     https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY
 git config --local user.email "$author_email"
 git config --local user.name "$GITHUB_ACTOR"
 
-# Commit changes and push to gh-pages
+# Commit changes and push to $GP_BRANCH
 git add --all .
-git fetch origin $GITHUB_SHA
-msg="[CI] Updating gh-pages branch from ${GITHUB_SHA:0:8}"
-if [[ "$orig_subject" == *full?linkcheck* ]]; then
-    msg="[full-linkcheck] $msg"
-fi
-git commit -q -m "$msg"
-git push -f origin gh-pages:gh-pages
+git commit -q -m "[CI] Updating $GP_BRANCH branch from ${GITHUB_SHA:0:8}"
+git push -f origin pages:$GP_BRANCH
