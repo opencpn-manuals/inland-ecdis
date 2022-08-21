5084117b8b69131f21a1d9dd83050c194fd34f29
Rasbats <miker@members.swis.net>
2022-08-21 16:12:12 +0100

    Update src-list.js

--- src-list.js
+++ src-list.js
@@ -1,15 +1,6 @@
 'use strict'
 
 const fs = require('fs')
-const git = requireGit()
+const git = require('@antora/content-aggregator') // Q: add dependency on isomorphic-git instead?
 
-
-function requireGit() {
-  return require(require.resolve("isomorphic-git", {
-    paths: [
-      require.resolve("@antora/content-aggregator", { paths: module.paths }) +
-        "/..",
-    ],
-  }));
-}
 
